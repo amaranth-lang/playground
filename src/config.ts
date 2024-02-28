89cbf9e4f51c2635d12fcee975636924c2dccaec
Catherine <whitequark@whitequark.org>
2024-02-28 15:30:37 +0000

    Update for Amaranth v0.4.3.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,10 @@
 export default {
-  amaranthVersions: ['v0.4.2'],
+  amaranthVersions: ['v0.4.3', 'v0.4.2'],
   pythonPackages: {
+    'v0.4.3': [
+      'https://files.pythonhosted.org/packages/98/8d/a0d8fb2b9611f3ae22ddc98890b346833fa2c645ad21fd282e61ccdad477/pyvcd-0.4.0-py2.py3-none-any.whl',
+      'https://files.pythonhosted.org/packages/72/34/82f76a59f4155e26f42cf9f9d04d80befe774aa8af6d9b6e48d3e7b9f060/amaranth-0.4.3-py3-none-any.whl',
+    ],
     'v0.4.2': [
       'https://files.pythonhosted.org/packages/98/8d/a0d8fb2b9611f3ae22ddc98890b346833fa2c645ad21fd282e61ccdad477/pyvcd-0.4.0-py2.py3-none-any.whl',
       'https://files.pythonhosted.org/packages/27/1c/39881fbd48f9de91d64955f206a7f32fd912d306d18e8c5f74126ee5962f/amaranth-0.4.2-py3-none-any.whl',
